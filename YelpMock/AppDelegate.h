9d9b48ade3fd8bc8ce3d67f1c3f3fcc77daa7d70
wang.xur <wang.xur@husky.neu.edu>
2017-10-10 19:06:11 -0700

    Initial Commit

---
+++ YelpMock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YelpMock
+//
+//  Created by Mockin_Aeon. on 10/10/17.
+//  Copyright © 2017 Mockin_Aeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YelpMock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YelpMock
+//
+//  Created by Mockin_Aeon. on 10/10/17.
+//  Copyright © 2017 Mockin_Aeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
